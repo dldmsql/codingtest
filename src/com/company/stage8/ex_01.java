bba5927290c81947a15fed18224392a47b1cccd7
dldmsql <yhy5913@gmail.com>
2022-01-20 21:55:25 +0900

    first commit

--- src/com/company/stage8/ex_01.java
+++ src/com/company/stage8/ex_01.java
@@ -1,4 +1,25 @@
 package com.company.stage8;
 
+import java.util.Scanner;
+
 public class ex_01 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(" ");
+        long a = Long.parseLong(input[0]); // 고정 비용 
+        long b = Long.parseLong(input[1]); // 가변 비용 - 1대 당
+        long c = Long.parseLong(input[2]); // 판매 가격 - 1대 당
+
+        System.out.println(calcualte(a,b,c));
+    }
+
+    private static long calcualte(long a, long b, long c) {
+        if( b >= c ){ // 1대 만드는 데 들어가는 가변 비용이 판매가격보다 크면, 남는 게 없다.
+            return -1;
+        } else {
+            long n = a / ( c - b); // 손익분기점
+            return n+1;
+        }
+    }
 }
---
+++ src/com/company/stage8/ex_02.java
@@ -0,0 +1,22 @@
+package com.company.stage8;
+
+import java.util.Scanner;
+
+public class ex_02 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 1; // 최소 루트의 개수 = 출력 변수
+        int range = 2; // 방이 2개 이상일테니까 range의 시작을 2로 둠
+
+        if(n == 1)
+            System.out.println(1);
+        else {
+            while(range <= n){
+                range = range + (6 * cnt); // 2, 8, 20, 38, ...
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
