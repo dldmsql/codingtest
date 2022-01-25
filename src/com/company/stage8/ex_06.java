032b8a4fe88958a346d5cb6ec8c7d287642eef8b
dldmsql <yhy5913@gmail.com>
2022-01-25 13:45:14 +0900

    first commit

--- src/com/company/stage8/ex_06.java
+++ src/com/company/stage8/ex_06.java
@@ -6,13 +6,28 @@ public class ex_06 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        int[] base = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
         for(int i =0; i < t; i++){
-           int k = sc.nextInt();
-           int n = sc.nextInt();
+            int k = sc.nextInt();
+            int n = sc.nextInt();
 
+            System.out.println(self(k,n));
 
         }
 
+    }
+    private static int self(int k, int n) {
+        // 층수 k
+        // 호수 n
+        int sum = 0;
+        if( k == 0 ) { // 0층일 경우,
+           for(int i =1; i<= n; i++){
+               sum = i; // i가 곧 호수이다.
+           }
+           return sum;
+        }
+        for(int i =1; i<=n; i++){ // 호수만큼 반복
+            sum += self(k-1, i); // i가 곧 호수이다. 재귀적으로 sum에 더해간다.
+        }
+        return sum;
     }
 }
