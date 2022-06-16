1f425d61bd1b2b0aaa3bc2752cf26b16a02d9eaa
dldmsql <yhy5913@naver.com>
2022-06-16 16:29:36 +0900

    study: Change word to num with programmers

---
+++ src/com/company/programmer/numAndWord.java
@@ -0,0 +1,22 @@
+package com.company.programmer;
+
+public class numAndWord {
+  public int solution(String s) {
+    int answer = 0;
+
+    /*
+    * 영어 -> 숫자로 바꿔라.
+    * */
+
+   String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+   for(int i =0; i< words.length; i++) {
+     if (s.contains(words[i])) {
+       s = s.replace(words[i],String.valueOf(i));
+     }
+   }
+
+    answer = Integer.valueOf(s);
+    return answer;
+  }
+}
