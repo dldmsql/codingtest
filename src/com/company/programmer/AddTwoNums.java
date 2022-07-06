c3081d9f88c8e7f7d847f90832e026cee916ff2e
dldmsql <yhy5913@naver.com>
2022-07-06 14:46:03 +0900

    study: greedy and hashset with programmers

---
+++ src/com/company/programmer/AddTwoNums.java
@@ -0,0 +1,18 @@
+package com.company.programmer;
+
+import java.util.HashSet;
+
+public class AddTwoNums {
+  public int[] solution(int[] numbers) {
+//    int[] answer = {};
+
+    HashSet<Integer> answer = new HashSet<>();
+
+    for ( int i =0; i < numbers.length; i++ ) {
+      for ( int j = i+1; j < numbers.length; j++) {
+        answer.add(numbers[i] + numbers[j]);
+      }
+    }
+    return answer.stream().sorted().mapToInt(Integer::intValue).toArray();
+  }
+}
