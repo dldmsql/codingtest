15204695eb83126b046e2d2178be4df76da31c37
dldmsql <yhy5913@naver.com>
2022-07-13 12:05:10 +0900

    fix: Edit error

--- src/com/company/programmer/IndexOfK.java
+++ src/com/company/programmer/IndexOfK.java
@@ -15,7 +15,7 @@ public class IndexOfK {
     * 인덱스 계산에 주의하자.
     * */
     for (int i =0; i< commands.length; i++) {
-      int[] temp = Arrays.copyOfRange(array, commands[i][0], commands[i][1]);
+      int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
 
       Arrays.sort(temp);
       answer[i] = temp[commands[i][2]-1];
